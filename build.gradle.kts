95694e7a71e4f82e788a6ace22f749e6c398cae3
ruskei <dominusfinem@gmail.com>
2025-10-08 13:27:02 -0700

    update to paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "2.0.0"
     id("com.gradleup.shadow") version "8.3.0"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
 }
 
 group = "com.ixume"
@@ -18,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 }
